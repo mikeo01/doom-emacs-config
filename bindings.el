ec0ceedd0222eaf3072b2a8b00b54563ae2fe935
Michael Long <Michael Longlongmichaeljev@icloud.com>
2023-04-14 15:55:18 +0100

    bindings

--- bindings.el
+++ bindings.el
@@ -10,7 +10,10 @@
 ;; ,q in vim speak
 (map! "C-x k" #'kill-this-buffer)
 
+;; easymotion overrides
 (map! :n "k" #'evilem-motion-previous-line)
 (map! :n "j" #'evilem-motion-next-line)
 (map! :n "l" #'evilem-motion-forward-word-begin)
 (map! :n "h" #'evilem-motion-backward-word-begin)
+
+(map! :n "K" #'lsp-ui-doc-toggle)
